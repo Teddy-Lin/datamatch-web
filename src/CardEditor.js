ab103eb1b27b910cc4d6d6c4b0ef1dcd824fb91f
Teddy-Lin <55064982+Teddy-Lin@users.noreply.github.com>
2020-11-24 15:41:48 +0800

    Tic Tac Toe and Flashcards

---
+++ src/CardEditor.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import './CardEditor.css';
+
+class CardEditor extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {front: '', back: ''}
+    }
+
+    // handleFrontChange = event => {
+    //     console.log(event.value.name);
+    //     this.setState({front: event.target.value});
+    // };
+
+    // handleBackChange = event => {
+    //     console.log(event.value.name);
+    //     this.setState({back: event.target.value});
+    // };
+
+
+    addCard = () => {
+        // if neither side is empty
+        if (this.state.front.trim() && this.state.back.trim()){
+            this.props.addCard(this.state);
+            this.setState({front: '', back: ''});
+        } 
+    };
+
+    // deleteCard = index => {
+    //     this.props.deleteCard(index);
+    // };
+    
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value});
+    };
+
+    render() {
+        const cards = this.props.cards.map((card, index) =>{
+            return (
+                <tr key = {index}>
+                    <td>{card.front}</td>
+                    <td>{card.back}</td>
+                    <td>
+                        <button onClick = {() => this.props.deleteCard(index)} disabled = {this.props.cards.length === 1}>
+                            Delete Card
+                        </button>
+                    </td>
+                </tr>
+            )
+        });
+
+        return(
+            <div>
+                <h2>Card Editor</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Front</th>
+                            <th>Back</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>{cards}</tbody>
+                </table>
+                <br/>
+                <input 
+                name = "front"
+                onChange = {this.handleChange} 
+                placeholder = "Front of Card" 
+                value = {this.state.front}
+                />
+                <input 
+                name = "back"
+                onChange = {this.handleChange} 
+                placeholder = "Back of Card" 
+                value = {this.state.back}
+                />
+                <button onClick = {this.addCard} disabled = {!this.state.front.trim() || !this.state.back.trim()}>Add Card</button>
+                <hr/>
+                <button onClick = {this.props.switchMode}>Go to Card Viewer</button>
+            </div>
+        )
+    }
+}
+
+export default CardEditor
---
+++ src/CardViewer.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './CardViewer.css'
+
+class CardViewer extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            front: true,
+            index: 0,
+        }
+    }
+
+    flip = () => this.setState({front: !this.state.front});
+    next = () => {
+        if (this.state.index < this.props.cards.length - 1) {
+            this.setState({
+                index: this.state.index + 1, 
+                front: true});
+        }
+    };
+
+    previous = () => {
+        if (this.state.index > 0) {
+            this.setState({
+                index: this.state.index - 1, 
+                front:true});
+        }
+    };
+        
+    
+    render(){
+        const cards = this.props.cards.map(card =>{
+            if (this.state.front) {
+                return (
+                    <tr>
+                        <td>{card.front}</td>
+                    </tr>
+                )
+            } else {
+                return (
+                    <tr>
+                        <td>{card.back}</td>
+                    </tr>
+                )
+            }
+            
+        });
+
+        return(
+            <div>
+                <h2>Card Viewer</h2>
+                <h4>You are viewing the {this.state.front? "front" : "back"} of card number {this.state.index + 1}</h4>
+                <h4>{this.state.index === this.props.cards.length - 1? "Congratulations! You've reached the end." : "There are still {this.props.cards.length - this.state.index - 1} cards left till you reach the end."}</h4>
+                <table>
+                    <tbody onClick = {() => this.flip()}>
+                        {cards[this.state.index]}
+                    </tbody>
+                </table>
+                {/* why three ===? */}
+                <button disabled = {this.state.index === 0} onClick = {() => this.previous()}>Previous</button>
+                <button disabled = {this.state.index === this.props.cards.length - 1} onClick = {() => this.next()}>Next</button>
+                <hr/>
+                <button onClick = {this.props.switchMode}>Go to Card Editor</button>
+            </div>
+        )
+    }
+}
+
+export default CardViewer;
