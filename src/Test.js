1063c4ed5fc3dc5b719cd8574bd8ae8e82619dc4
Teddy-Lin <55064982+Teddy-Lin@users.noreply.github.com>
2020-12-03 22:04:30 +0800

    connect from react-redux

--- src/Test.js
+++ src/Test.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom';
+// import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
-import {firebaseConnect, isLoaded} from 'react-redux-firebase';
-import {compose} from 'redux';
+// import {firebaseConnect, isLoaded} from 'react-redux-firebase';
+// import {compose} from 'redux';
 
-const Test = (props) => {
+const Test = props => {
     // return <div>{props.flashcards.deck1.name}</div>
-    return <div>Test {props.match.params.id}</div>
-    // return <div>Test</div>
-}
+    // return <div>Test {props.match.params.id}</div>
+    return <div>Test</div>;
+};
 
-// const mapStateToProps = state => {
+ const mapStateToProps = state => {
 //     if (!isLoaded(props.flashcards)){
 //         return <div>Loading...</div>;
 
 //     }
-//     console.log(state);
+     console.log(state);
+     const isEmpty = state.firebase.profile.isEmpty;
+     
 //     const flashcards = state.firebase.data.flashcards;
-//     return {test: '1234', hello: 'world', isEmpty: isEmpty};
-// };
+     return {test: '1234', hello: 'world', isEmpty: isEmpty};
+ };
 
 
 // // export default firebaseConnect([/flashcards])(connect(mapStateToProps)(Test));
@@ -28,4 +30,5 @@ const Test = (props) => {
 // )(Test);
 
 // export default Test;
-export default withRouter(Test);
+// export default withRouter(Test);
+export default connect(mapStateToProps)(Test);
