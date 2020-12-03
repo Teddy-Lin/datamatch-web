ea7b7b8529cc35d7baa76451d6031ab4babc8608
Teddy-Lin <55064982+Teddy-Lin@users.noreply.github.com>
2020-12-03 22:51:43 +0800

    firebaseConnect from react-redux-firebase

--- src/Test.js
+++ src/Test.js
@@ -1,34 +1,31 @@
 import React from 'react';
 // import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
-// import {firebaseConnect, isLoaded} from 'react-redux-firebase';
-// import {compose} from 'redux';
+import {firebaseConnect, isLoaded} from 'react-redux-firebase';
+import {compose} from 'redux';
 
 const Test = props => {
-    // return <div>{props.flashcards.deck1.name}</div>
+    if (!isLoaded(props.flashcards)){
+        return <div>Loading...</div>;
+    }
+    
     // return <div>Test {props.match.params.id}</div>
-    return <div>Test</div>;
+    // return <div>Test</div>;
+    return <div>{props.flashcards.deck1.name}</div>;
 };
 
  const mapStateToProps = state => {
-//     if (!isLoaded(props.flashcards)){
-//         return <div>Loading...</div>;
-
-//     }
      console.log(state);
-     const isEmpty = state.firebase.profile.isEmpty;
-     
-//     const flashcards = state.firebase.data.flashcards;
-     return {test: '1234', hello: 'world', isEmpty: isEmpty};
+    //  const isEmpty = state.firebase.profile.isEmpty;
+     const flashcards = state.firebase.data.flashcards;
+    //  return {test: '1234', hello: 'world', isEmpty: isEmpty};
+    return {flashcards: flashcards};
  };
 
 
-// // export default firebaseConnect([/flashcards])(connect(mapStateToProps)(Test));
-// export default compose(
-//     firebaseConnect([/flashcards]),
-//     connect(mapStateToProps),
-// )(Test);
-
 // export default Test;
 // export default withRouter(Test);
-export default connect(mapStateToProps)(Test);
+// export default connect(mapStateToProps)(Test);
+// export default firebaseConnect(['/flashcards'])(Test);
+// export default firebaseConnect(['/flashcards'])(connect(mapStateToProps)(Test));
+export default compose(firebaseConnect(['/flashcards']), connect(mapStateToProps))(Test);
